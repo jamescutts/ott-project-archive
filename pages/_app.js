27ab71ed7f4b3333abdd3adac94d55739fcd9f28
James Cutts <james@jamescutts.co.uk>
2023-06-02 16:45:30 +0100

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
-import { clarity } from 'react-microsoft-clarity';
 import { useEffect } from 'react'
+import { clarity } from 'react-microsoft-clarity';
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
 
   useEffect(() => {
     clarity.init("hdnbx1x1qc");
   })
-  return <Component {...pageProps} />
+  return <>
+    <GoogleAnalytics trackPageViews />
+    <Component {...pageProps} />
+  </>
 }
